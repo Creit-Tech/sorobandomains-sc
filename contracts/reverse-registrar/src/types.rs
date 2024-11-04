96179a861e6bf3e07e6839dbd4e523bcd9af087d
Jun Luo <4catcode@gmail.com>
2024-11-05 07:19:00 +0800

    Use comments in the contract type to document and reduce the size of wasm.

--- contracts/reverse-registrar/src/types.rs
+++ contracts/reverse-registrar/src/types.rs
@@ -1,18 +1,18 @@
 use soroban_sdk::{contracttype, Bytes, Vec};
 
-/// Represents a domain structure with a top-level domain (TLD),
-/// second-level domain (SLD), and a vector of subdomains.
+// Represents a domain structure with a top-level domain (TLD),
+// second-level domain (SLD), and a vector of subdomains.
 #[contracttype]
 pub struct Domain {
-    /// The top-level domain (TLD), e.g., "xlm" in `a.b.example.xlm`.
+    // The top-level domain (TLD), e.g., "xlm" in `a.b.example.xlm`.
     pub tld: Bytes,
 
-    /// The second-level domain (SLD), e.g., "example" in `a.b.example.xlm`.
+    // The second-level domain (SLD), e.g., "example" in `a.b.example.xlm`.
     pub sld: Bytes,
 
-    /// A list of subdomains, e.g., ["a", "b"] in `a.b.example.xlm`.
-    /// Currently, the maximum number of subdomains is 1, but more can be added in the future.
-    /// Keep it empty if there are no subdomains.
+    // A list of subdomains, e.g., ["a", "b"] in `a.b.example.xlm`.
+    // Currently, the maximum number of subdomains is 1, but more can be added in the future.
+    // Keep it empty if there are no subdomains.
     pub subs: Vec<Bytes>,
 }
 
